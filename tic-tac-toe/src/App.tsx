21e88a23071f50b76107a619d9f2c5105cfff722
Catalin <geaigelsaflu@gmail.com>
2023-08-28 14:12:42 +0300

    finished design

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -13,10 +13,20 @@ function App() {
         <div className="container text-center bg-dark">
           <h1 className="text-white">Tic Tac Toe</h1>
           <GameTable></GameTable>
-          <h2 className={turn === 1 ? "green" : "red"}>{turn === 1 ? "Green's turn." : "Red's turn."}</h2>
+          <h2 className={turn === 1 ? "green" : "red"}>
+            {turn === 1 ? "Green's turn." : "Red's turn."}
+          </h2>
           <div className="container text-center score">
             <h2 className="red">Red : {player1}</h2>
             <h2 className="green">Green : {player2}</h2>
+          </div>
+          <div className="container text-center menu">
+            <button type="button" className="btn btn-secondary btn-custom">
+              <h3>Reset Table</h3>
+            </button>
+            <button type="button" className="btn btn-warning btn-custom">
+            <h3>Reset Score</h3>
+            </button>
           </div>
         </div>
       </div>
