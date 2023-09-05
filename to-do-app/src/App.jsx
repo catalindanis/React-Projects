7d1a87618383da27073345c2eec68f92e2c9eccf
Catalin <geaigelsaflu@gmail.com>
2023-09-05 13:05:02 +0300

    Added functionality to buttons

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -4,26 +4,48 @@ import viteLogo from "/vite.svg";
 import Task from "./Components/Task";
 import AddTask from "./Components/AddTask";
 
+export let task = {
+  title: "",
+  description: ""
+}
+
+export let modify = false;
+
+export let currentTasks = [];
+
 function App() {
+  const [tasksState, setTasksState] = useState([]); 
+
   return (
     <>
-      <div clasName="container-fluid">
+      <div className="container-fluid">
         <div className="container m-auto">
           <div className="text-center">
             <h1>Task-list</h1>
           </div>
-          <ul class="list-group">
-            <li class="list-group-item custom-item">
-              <Task></Task>
-            </li>
+          <ul className="list-group">
+            {currentTasks.length == 0 ? (
+              <h6 className="text-center">Tasks will be shown here</h6>
+            ) : (
+                tasksState.map(item => <li key = {item.id}>{item.value}</li>)
+            )}
           </ul>
           <div className="text-center">
-            <button type="button" id="1" className="btn btn-dark w-50 custom-button">
+            <button
+              type="button"
+              id="1"
+              className="btn btn-dark w-50 custom-button"
+              onClick={() => {
+                document.getElementById("add-task").style.display = "block";
+              }}
+            >
               <h4>Add task</h4>
             </button>
           </div>
         </div>
-        <AddTask></AddTask>
+        <div id="add-task">
+          <AddTask></AddTask>
+        </div>
       </div>
     </>
   );
--- to-do-app/src/Components/AddTask.jsx
+++ to-do-app/src/Components/AddTask.jsx
@@ -1,20 +1,39 @@
+import { task, currentTasks, modify } from "../App";
+
 function AddTask() {
   return (
     <>
       <div className="full-size">
         <div className="align-center custom-container">
-          <div className="form-group">
-          <div className="close-button">
-            <h2>X</h2>
+          <div
+            className="close-button"
+            onClick={() => {
+              closeWindow();
+            }}
+          >
+            <h2 className="disable-text">X</h2>
           </div>
+          <div className="form-group">
             <h1 className="form-title">Title</h1>
-            <input type="text" className="w-100" />
+            <input id="title" type="text" className="w-100 custom-font" />
           </div>
           <div className="form-group">
             <h1 className="form-title">Description</h1>
-            <input type="text" className="w-100" />
+            <input id="description" type="text" className="w-100 custom-font" />
           </div>
-          <button type="button" className="btn btn-warning custom-button">
+          <button
+            type="button"
+            className="btn btn-warning custom-button"
+            onClick={() => {
+              currentTasks.push({
+                title: document.getElementById("title").value,
+                description: document.getElementById("description").value,
+              });
+              
+
+              closeWindow();
+            }}
+          >
             <h3>Add task</h3>
           </button>
         </div>
@@ -24,3 +43,11 @@ function AddTask() {
 }
 
 export default AddTask;
+
+function closeWindow() {
+  document.getElementById("add-task").style.animation = "hide 0.2s";
+  setTimeout(() => {
+    document.getElementById("add-task").style.animation = "load 0.2s";
+    document.getElementById("add-task").style.display = "none";
+  }, 200);
+}
